cafccf599836af7f1d468cc4175191fe69ee8a76
FrankKeyserSoze <keyser_soze@outlook.com>
2020-06-24 20:10:19 +0900

    敵の体力　攻撃食らった時のアニメーション編集中

---
+++ Assets/MyScripts/BoneEnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneEnemyController : MonoBehaviour
+{
+    public GameObject player;
+    float boneHp = 100f;
+    public float twoHWADam;
+    public float rLegDam;
+    int twoHWACriticalRate;
+    public float twoHWACriticalDam;
+    int rLegCriticalRate;
+    public float rLegCriticalDam;
+    Animator animator;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(boneHp);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("2HWATag"))
+        {
+            twoHWACriticalRate = Random.Range(1,11);
+            if(twoHWACriticalRate <= 3)
+            {
+                twoHWADam *= twoHWACriticalDam;
+            }
+            else
+            {
+                twoHWADam = 30f;
+            }
+            boneHp -= twoHWADam;
+        }
+        if(other.gameObject.CompareTag("RLegTag"))
+        {
+            rLegCriticalRate = Random.Range(1,11);
+            if(rLegCriticalRate <= 5)
+            {
+                rLegDam *= rLegCriticalDam;
+            }
+            else
+            {
+                rLegDam = 15f;
+            }
+            boneHp -= rLegDam;
+            
+            animator.SetTrigger("HitTrigger");
+        }
+    }
+}
--- Assets/MyScripts/PlayerController.cs
+++ Assets/MyScripts/PlayerController.cs
@@ -13,22 +13,20 @@ public class PlayerController : MonoBehaviour
     Vector3 diff;
     Vector3 diffY0;
     float diffVero;
-    Vector3 antiWalkForce;
     AnimatorStateInfo info;
     Vector3 nlzForce;
     bool isAttack;
     public GameObject twoHWU;
     public GameObject twoHWA;
     public GameObject rLeg;
-    bool isMove;
-    public float twoHWADam;
-    public float rLegDam;
     bool isAttackDone;
+    bool isArmed;
+  
     // Start is called before the first frame update
     void Start()
     {
-        this.rigidbody = gameObject.GetComponent<Rigidbody>();
-        this.animator = GetComponent<Animator>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         twoHWA.SetActive(false);
     }
     // Update is called once per frame
@@ -39,6 +37,7 @@ public class PlayerController : MonoBehaviour
         diffVero = rigidbody.velocity.magnitude - lastVero;
         animator.SetFloat("Speed", lastVero);
         info = animator.GetCurrentAnimatorStateInfo(0);
+
         //AttackTagが付いているStateのとき攻撃中の旗を揚げる
         if(info.IsTag("Attack"))
         {
@@ -48,15 +47,14 @@ public class PlayerController : MonoBehaviour
         {
             isAttack = false;
         }
+
         //両手剣での攻撃モーション中
         if(info.IsName("2Hand-Sword-Attack2"))
         {
-            // StartCoroutine(ColliderEnabled());
-            // Debug.Log("Coroutine 開始");
+        
             if(isAttackDone == false)
             {
                 twoHWA.GetComponent<CapsuleCollider>().enabled = true;
-
             }
             else
             {
@@ -66,6 +64,7 @@ public class PlayerController : MonoBehaviour
         else
         {
             twoHWA.GetComponent<CapsuleCollider>().enabled = false;
+            isAttackDone = false;
         }
         //キックでの攻撃モーション中
         if(info.IsName("Hikick"))
@@ -79,71 +78,70 @@ public class PlayerController : MonoBehaviour
         //AttackTagがついているStateが進行してる時は無視
         if(isAttack == false)
         {
+            //攻撃
             if(Input.GetMouseButtonDown(0))
             {
                 animator.SetTrigger("AttackTrigger");
             }
-            if(isMove == false)
-            {
-                if(Input.GetKeyDown("1"))
+
+            //動いていない時
+                if(isArmed == false)
                 {
-                    animator.SetTrigger("Wepon1Trigger");
-                    StartCoroutine(SwitchWeapon());
+                    //武器交換
+                    if(Input.GetKeyDown("1"))
+                    {
+                        animator.SetTrigger("Wepon1Trigger");
+                        StartCoroutine(SwitchWeapon());
+                        isArmed = true;
+                    }
                 }
-            }
+                else
+                {
+                    //武器交換
+                    if(Input.GetKeyDown("1"))
+                    {
+                        animator.SetTrigger("Wepon1Trigger");
+                        StartCoroutine(SwitchWeapon());
+                        isArmed = false;
+                    }
+                }
+
+            //空中にいない時に入力されたら
             if(Input.GetKeyDown(KeyCode.Space) && isAir == false)
             {
                 this.rigidbody.AddForce(transform.up * this.jumpForce);
+
                 //空中にいるとき旗揚げ
                 isAir = true;
+
                 this.animator.SetTrigger("JumpTrigger");
-                this.rigidbody.AddForce(antiWalkForce);
             }
         }
         else
         {
             return;
         }
+
+        Debug.Log(isArmed);
+
     }
     
     
     void FixedUpdate()
     {
-        // 速度制限
-        // if(this.rigidbody.velocity.magnitude < this.maxWalkSpeed)
-        // {
-            //移動
-            // if(Input.GetKey(KeyCode.W))
-            // {
-            //     this.rigidbody.AddForce(transform.forward * this.walkForce);
-            // }
-            // if(Input.GetKey(KeyCode.S))
-            // {
-            //     this.rigidbody.AddForce(transform.forward * this.walkForce * -1f);
-            // }
-            // if(Input.GetKey(KeyCode.D))
-            // {
-            //     this.rigidbody.AddForce(transform.right * this.walkForce);
-            // }
-            // if(Input.GetKey(KeyCode.A))
-            // {
-            //     this.rigidbody.AddForce(transform.right * this.walkForce * -1f);
-            // }
-        // }
-        //VerticalとHorizontalAxisをそれぞれ前進、回転動作につなげる
-        // float translation = Input.GetAxisRaw("Vertical") * speed;
-        // float rotation = Input.GetAxisRaw("Horizontal") * rotationSpeed;
-        // //Z軸へ前進
-        // transform.Translate(0, 0, translation);
-        // // Y軸を中心に回転
-        // // transform.Rotate(0,rotation, 0);
-        // //速度、回転を10f/s,100f/sにする
-        // translation *= Time.deltaTime;
-        // rotation *= Time.deltaTime;
         
         if(isAttack == true)
         {
             return;
+        }
+
+        if(isArmed == true)
+        {
+            maxWalkSpeed = 1.5f;
+        }
+        else
+        {
+            maxWalkSpeed = 2.0f;
         }
          
         // 速度制限と攻撃中でなかったら実行
@@ -157,17 +155,19 @@ public class PlayerController : MonoBehaviour
             //ベクトルを正規化
             nlzForce = force.normalized;
             //正規化されたベクトルと歩く力を計算する
-            nlzForce *= this.walkForce;
+            nlzForce *= walkForce;
             // 物体に力を加える
-            this.rigidbody.AddForce(nlzForce);
+            rigidbody.AddForce(nlzForce);
         }
+
         //1フレーム前のPlayerの位置との差を計り、どの方向に進んでいるかを計算する。
-        this.diff = transform.position - lastPos;
+        diff = transform.position - lastPos;
         // キャラクターが上を向かないようにするためdiffのy軸方向のベクトルを0にする
-        this.diffY0 = this.diff;
-        this.diffY0.y = 0;
+        diffY0 = diff;
+        diffY0.y = 0;
         //transform.positionの値を代入する動作を含むことによって、ここの値は、1つ前のフレームのものになる。
         lastPos = transform.position;
+
         // 少し動いたら
         if(diffY0.magnitude > 0.01f)
         {
@@ -175,12 +175,8 @@ public class PlayerController : MonoBehaviour
             Quaternion playerRotation = Quaternion.LookRotation(diffY0);
             //最初に向いていた角度から差の方向へフレームごとに25%づつ回転する
             transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, 0.25f);
-            isMove = true;
         }
-        else
-        {
-            isMove = false;
-        }       
+        
         
     }
     void OnCollisionEnter(Collision other)
@@ -191,28 +187,21 @@ public class PlayerController : MonoBehaviour
             isAir = false;
         }
     }
+
+    //武器を手に持つモーションが始まってから背中の武器と手の武器が重なるまで待つ
     IEnumerator SwitchWeapon()
     {
         yield return new WaitForSeconds(0.38f);
         twoHWA.SetActive(!twoHWA.activeSelf);
         twoHWU.SetActive(!twoHWU.activeSelf);
     }
-    // IEnumerator ColliderEnabled()
-    // {
-    //     yield return new WaitForSeconds(0.4f);
-    //     twoHWA.GetComponent<CapsuleCollider>().enabled = false;
-    //     // Debug.Log(twoHWA.GetComponent<CapsuleCollider>().enabled);  
-    // }
+   
+    //両手剣攻撃モーションのEventで呼ばれる
     public void AttackDone()
     {
         isAttackDone = true;
     }
-    // void Sample()
-    // {
-    //     info = animator.GetCurrentAnimatorClipInfo(0);
-    //     for(int i = 0; i < info.Length; i++)
-    //     {
-    //         Debug.Log(info[i].clip.name);
-    //     }
-    // }
+
+   
+    
 }
